a2822256c8760bd79509c92844cddc8cd1ac79d2
Jeremy Gart <jgart20@jgart20.local>
2020-01-23 09:10:38 -0700

    added a pug file to thorns path

--- routes/thorns.js
+++ routes/thorns.js
@@ -7,6 +7,7 @@ router.use('/red', redRouter);
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('this is the Thorns pathway');
+  res.render('thorns', {name: 'PrickyBoi'})
 });
 
 module.exports = router;
