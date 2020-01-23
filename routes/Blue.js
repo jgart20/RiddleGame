23456ed2defa9a85e41db804e2a83eba8f74ad4a
Jake <jwriter20@kentdenver.org>
2020-01-23 08:44:27 -0700

    added the bootstrap link to the layout

--- routes/Blue.js
+++ routes/Blue.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('this is the blue route');
+  res.send('This is the blue route');
 });
 
 module.exports = router;
