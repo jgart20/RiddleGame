608fb48359a161668c2b72032478bf4a6faf29ec
Jeremy Gart <jgart20@jgart20.local>
2020-01-24 12:29:38 -0700

    worked on index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dashboard');
+  res.render('index', {title:'RiddleGame', name:'StarterBoi'});
 });
 
 module.exports = router;
